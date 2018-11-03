21716b653496321fb4a077fc354d9c38a69e2905
Hassan Nasser <hmarafih@gmail.com>
2018-11-03 16:40:45 +0300

    Initial Commit

---
+++ Scene/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Scene
+//
+//  Created by Hassan Marafih on 11/3/18.
+//  Copyright © 2018 Hassan Marafih. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Scene/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Scene
+//
+//  Created by Hassan Marafih on 11/3/18.
+//  Copyright © 2018 Hassan Marafih. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
